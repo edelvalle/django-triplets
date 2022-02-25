fa21401cd91af8518101462f4557edc8b2e02553
Eddy Ernesto del Valle Pino <eddy@edelvalle.me>
2022-02-25 19:52:18 +0100

    Configure the tests to use a local file when not runing in test mode

--- tests/tests/settings.py
+++ tests/tests/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "tests.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
+        "NAME": ":memory:" if "test" in sys.argv else "db.sqlite3",
     }
 }
 
