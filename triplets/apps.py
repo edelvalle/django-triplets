74cf0e4457d966a11dcecd31bbc7d85276688182
Eddy Ernesto del Valle Pino <eddy@edelvalle.me>
2022-02-25 17:29:02 +0100

    Minor app changes

--- triplets/apps.py
+++ triplets/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TripletsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'triplets'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "triplets"
