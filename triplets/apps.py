2fb33a035c221084e03ac837658ba184d5beab3d
Eddy Ernesto del Valle Pino <eddy@edelvalle.me>
2022-02-25 20:11:43 +0100

    Refresh inferences after migrations finish

--- triplets/apps.py
+++ triplets/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def refresh_inference(sender, **kwargs):
+    from . import api
+
+    api.refresh_inference()
 
 
 class TripletsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "triplets"
+
+    def ready(self):
+        post_migrate.connect(refresh_inference, sender=self)
