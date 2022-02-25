066a22d7586b1991ff0fde2f85a621d4de1156e3
Eddy Ernesto del Valle Pino <eddy@edelvalle.me>
2022-02-25 17:26:36 +0100

    Add an admin interface to look at stored triplets

--- triplets/admin.py
+++ triplets/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.StoredTriplet)
+class StoredTripletAdmin(admin.ModelAdmin):
+    search_fields = ["subject", "verb", "obj"]
+    list_display = search_fields + ["is_inferred"]
+
+    # Don't use this interface to mess around with triplets, use the API
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
